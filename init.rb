6d01fe650e09c0862e259dde789f74529da3f942
rubynovich <roman@shipiev.me>
2013-05-17 14:38:44 +0400

    fix: url on init.rb

--- init.rb
+++ init.rb
@@ -5,7 +5,6 @@ Redmine::Plugin.register :redmine_delays do
   author "Roman Shipiev"
   description 'Plugin fixes delays of employees'
   version '0.0.1'
-  url 'https://github.com/rubynovich/redmine_delays'
   url 'https://bitbucket.org/rubynovich/redmine_delays'
   author_url 'http://roman.shipiev.me/'
 
