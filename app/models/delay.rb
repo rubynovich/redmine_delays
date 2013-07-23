81ba9f6a36ce46ab7de4e0caaff98f407bc8fec9
Alexander Tkachev <teksisto@gmail.com>
2013-07-23 14:33:44 +0400

    fix: time_period scope moved to separate file. Еще не модуль, уже не хардкод.

--- app/models/delay.rb
+++ app/models/delay.rb
@@ -19,33 +19,6 @@ class Delay < ActiveRecord::Base
     where(field => q) if q.present? && field.present?
   }
 
-  scope :time_period, lambda {|q, field|
-    today = Date.today
-    if q.present? && field.present?
-      period_start, period_end = case q
-                                 when "yesterday"
-                                   [ 1.day.ago.beginning_of_day, 1.day.ago.end_of_day ]
-                                 when "today"
-                                   [ today.beginning_of_day, today.end_of_day ]
-                                 when "last_week"
-                                   [ 1.week.ago.beginning_of_week, 1.week.ago.end_of_week ]
-                                 when "this_week"
-                                   [ today.beginning_of_week, today.end_of_week]
-                                 when "last_month"
-                                   [1.month.ago.beginning_of_month, 1.month.ago.end_of_month]
-                                 when "this_month"
-                                   [today.beginning_of_month, today.end_of_month]
-                                 when "last_year"
-                                   [1.year.ago.beginning_of_year, 1.year.ago.end_of_year ]
-                                 when "this_year"
-                                   [today.beginning_of_year, today.end_of_year ]
-                                 end
-
-      {:conditions => ["#{field} BETWEEN ? AND ?", period_start , period_end] }
-     
-    end
-  }
-
   def to_s
     user ? user.name : ""
   end
--- init.rb
+++ init.rb
@@ -13,4 +13,14 @@ Redmine::Plugin.register :redmine_delays do
     :caption => :label_delay_plural,
     :after => :vacation_statuses,
     :if => Proc.new{ User.current.is_vacation_manager?}
+
+
+end
+
+Rails.configuration.to_prepare do
+  require_dependency 'delay'
+  require 'time_period_scope'
+  unless Delay.included_modules.include? TimePeriodScope
+    Delay.send( :include, ) 
+  end
 end
